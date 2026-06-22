5da02cb52408ccfae16bd7ac683a69dcdee8643e
Elhadji Abdoulaye Niass <mr.niass@gmail.com>
2026-06-22 13:10:53 +0000

    Remove unused assets and orphaned components

--- public/scripts/theme-init.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  try {
-    var theme = localStorage.getItem('hb-theme');
-    if (theme === 'dark' || (!theme && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-      document.documentElement.classList.add('dark');
-    }
-  } catch(e) {}
-})();
